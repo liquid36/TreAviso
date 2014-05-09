39656ac2015000aefa44191dbfe81f5a6e29c786
Mariano Botta <marianoabotta@gmail.com>
2014-05-09 17:16:53 -0300

    Agrego el ejemplo de backgroundposition No salta el deviceready: Hay que averiguarlo

--- www/js/index.js
+++ www/js/index.js
@@ -26,7 +26,8 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+        console.log('BackgroundGeoLocation ');
+        document.addEventListener('deviceready', this.onDeviceReady, true);
     },
     // deviceready Event Handler
     //
@@ -34,6 +35,11 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+	console.log('--------   device ready');
+        if (window.plugins.backgroundGeoLocation) {
+            app.configureBackgroundGeoLocation();
+        }
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -45,5 +51,65 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
+    },
+    configureBackgroundGeoLocation: function() {
+        // Your app must execute AT LEAST ONE call for the current position via standard Cordova geolocation,
+        //  in order to prompt the user for Location permission.
+        window.navigator.geolocation.getCurrentPosition(function(location) {
+            console.log('Location from Phonegap');
+        });
+
+        var bgGeo = window.plugins.backgroundGeoLocation;
+
+        /**
+        * This would be your own callback for Ajax-requests after POSTing background geolocation to your server.
+        */
+        var yourAjaxCallback = function(response) {
+            ////
+            // IMPORTANT:  You must execute the #finish method here to inform the native plugin that you're finished,
+            //  and the background-task may be completed.  You must do this regardless if your HTTP request is successful or not.
+            // IF YOU DON'T, ios will CRASH YOUR APP for spending too much time in the background.
+            //
+            //
+            bgGeo.finish();
+        };
+
+        /**
+        * This callback will be executed every time a geolocation is recorded in the background.
+        */
+        var callbackFn = function(location) {
+            console.log('[js] BackgroundGeoLocation callback:  ' + location.latitudue + ',' + location.longitude);
+            // Do your HTTP request here to POST location to your server.
+            //
+            //
+            yourAjaxCallback.call(this);
+        };
+
+        var failureFn = function(error) {
+            console.log('BackgroundGeoLocation error');
+        }
+        
+        // BackgroundGeoLocation is highly configurable.
+        bgGeo.configure(callbackFn, failureFn, {
+            url: 'http://only.for.android.com/update_location.json', // <-- only required for Android; ios allows javascript callbacks for your http
+            params: {                                               // HTTP POST params sent to your server when persisting locations.
+                auth_token: 'user_secret_auth_token',
+                foo: 'bar'
+            },
+            desiredAccuracy: 10,
+            stationaryRadius: 20,
+            distanceFilter: 30,
+            debug: true // <-- enable this hear sounds for background-geolocation life-cycle.
+        });
+
+        // Turn ON the background-geolocation system.  The user will be tracked whenever they suspend the app.
+        bgGeo.start();
+	console.log('BackgroundGeoLocation ');
+	console.log('BackgroundGeoLocation ');
+	console.log('BackgroundGeoLocation ');
+	console.log('BackgroundGeoLocation ');
+        // If you wish to turn OFF background-tracking, call the #stop method.
+        // bgGeo.stop()
     }
+
 };
