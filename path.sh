25cc238b25ceab5e952b98768cc971a90af84e8f
Mariano Botta <marianoabotta@gmail.com>
2014-05-15 19:18:01 -0300

    Cambio Nombres

---
+++ path.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#esto podria ir en el bash al inicio del sistema
+JAVA_HOME=/usr/lib/jvm/java-1.7.0-openjdk-i386/
+PATH=$PATH:/home/sam/Programacion/andriod/sdk/tools/:/home/sam/Programacion/andriod/sdk/platform-tools/
+export PATH
+export JAVA_HOME
+
+
+#ln -s /usr/bin/nodejs /usr/bin/nodex
+#http://developer.android.com/tools/device.html
+
